8d835f5bafcc9438c12e81d3ac481dba7e601550
bherbin <33049464+bherbin@users.noreply.github.com>
2017-11-04 21:48:01 -0400

    First website upload

---
+++ index_htm_files/2.js
@@ -0,0 +1,13 @@
+window.local_mosaic_slide_91_data = [
+{
+ direction: "0",
+ speed: "0.5",
+ theme: "0"
+},
+{
+ heading: "index_htm_files/2822.png",
+ image: "index_htm_files/2826.jpg",
+ link: "#",
+ text: "index_htm_files/2827.png"
+}
+];
